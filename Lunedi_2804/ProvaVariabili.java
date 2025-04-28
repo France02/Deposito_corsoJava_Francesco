dc36a9b7d6be789d0d94dc99568a0eb9e0901d3b
Francesco Golino <francescogolino02@gmail.com>
2025-04-28 15:47:28 +0200

    Agginto

--- Lunedi_2804/ProvaVariabili.java
+++ Lunedi_2804/ProvaVariabili.java
@@ -1,9 +1,24 @@
-public class ProvaVariabili {
+public class ProvaVariabili {   
     public static void main(String[] args) {
-        System.out.println(3);
-        System.out.println(358);
-        System.out.println(50000);
+        // Stampa i numeri nella console
+        System.out.println(3);  
         System.out.println(3 + 3);
         System.out.println(3*8);
+
+        int provaNumero = 12;   // Varie prove con le varibili
+        String provaTesto = " Topolino";
+        Boolean provaBoolean = true;
+        
+        System.out.println("Il suo nome è " + provaTesto);
+        String fullPart = provaNumero + provaTesto; //Unire sia int + Sting
+        System.out.println(fullPart);
+
+        //Per i valori numeri il + funziona come un operatore matematico
+        int num1 = 5;
+        int num2 = 10;
+        System.out.println(x + y);
+
+        int num_c1 = 10, num_c2 = 20, num_c3 = 30;  
+        System.out.println(num_c1 + num_c2 + num_c3);
     }
 }
--- Lunedi_2804/UseArgument.java
+++ Lunedi_2804/UseArgument.java
@@ -1,6 +1,6 @@
 public class UseArgument {
     public static void main(String[] args) {
-        String pippo = "Pippo";
+        String pippo = "Pippo"; // Abbiamo inserito pippo in una variabi
         System.out.print("Hi, ");
         System.out.print(pippo);
         System.out.println(". How are you?");
