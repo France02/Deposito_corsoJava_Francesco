9016724d347674e3eca4add55fe6b3565a80e2ca
Francesco_Golino <133333709+France02@users.noreply.github.com>
2025-04-28 17:55:37 +0200

    Aggiunti i due esercizi

--- JAVA/Lunedi_2804/Esercizio1.java
+++ JAVA/Lunedi_2804/Esercizio1.java
@@ -1,3 +1,34 @@
+import java.util.Scanner;
+
 public class Esercizio1 {
-    
+    public static void main(String[] args) {
+        //CREAZIONE DELL'OGGETTO SCANNER PER LA LETTURA
+       Scanner myObj = new Scanner(System.in);
+       
+       // LETTURA TIPI CHAR E STRING 
+       System.out.println("Tipo char e String");
+        String stringChar = myObj.nextLine();
+        System.out.println("Dato:" + stringChar);
+
+        // LETTURA TIPO BYTE 
+       System.out.println("Tipo Byte");
+        byte myBite = myObj.nextByte();
+        System.out.println("Dato:" + myBite);
+
+        // LETTURA TIPO BOOLEANO 
+       System.out.println("Tipo Boolean");
+        boolean valoreBool = myObj.nextBoolean();
+        System.out.println("Dato:" + valoreBool);
+
+        // LETTURA TIPO DOUBLE 
+       System.out.println("Tipo Double");
+        double myDouble = myObj.nextDouble();
+        System.out.println("Dato:" + myDouble);
+
+        // LETTURA TIPO FLOAT 
+       System.out.println("Tipo float");
+        float myFloat = myObj.nextFloat();
+        System.out.println("Dato:" + myFloat);
+        
+    }
 }
