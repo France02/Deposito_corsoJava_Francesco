6bc9b2e73b13bda3242ff2269df6a5b72211ad90
Francesco Golino <francescogolino02@gmail.com>
2025-04-29 09:57:48 +0200

    Aggiunta Cartella Martedi

---
+++ Martedi_2904/ProvaStringe.java
@@ -0,0 +1,6 @@
+public class ProvaStringe {
+    public static void main(String[] args) {
+        String text = "Francesco";
+        System.out.println("la lunghezza: " + text.length());
+    }
+}
