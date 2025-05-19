abd52e4a2580f4011183c60ffa8426210e3ae9f5
Francesco_Golino <133333709+France02@users.noreply.github.com>
2025-05-19 15:31:56 +0200

    Aggiunto interrogazione al db con i 10 film piu noleggiati

--- Lunedi_1905/src/DatabaseConnection.java
+++ Lunedi_1905/src/DatabaseConnection.java
@@ -15,6 +15,33 @@ public class DatabaseConnection {
             // Connessione al database
             Connection conn = DriverManager.getConnection(url, user, password);
             System.out.println("Connessione riuscita!");
+            String sql = """
+                            SELECT 
+                                f.title AS film_title,
+                                COUNT(r.rental_id) AS total_rentals
+                            FROM 
+                                rental r
+                            JOIN 
+                                inventory i ON r.inventory_id = i.inventory_id
+                            JOIN 
+                                film f ON i.film_id = f.film_id
+                            GROUP BY 
+                                f.film_id, f.title
+                            ORDER BY 
+                                total_rentals DESC
+                            LIMIT 10;
+                            """;
+
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+
+             System.out.println("\n10 Film più noleggiati:");
+                while (rs.next()) {
+                    String title = rs.getString("film_title");
+                    int rentals = rs.getInt("total_rentals");
+                    System.out.println(title + " - " + rentals + " noleggi");
+                }
+
         } catch (ClassNotFoundException e) {
             System.err.println("Driver JDBC non trovato. Assicurati di aver aggiunto il JAR.");
             e.printStackTrace();
